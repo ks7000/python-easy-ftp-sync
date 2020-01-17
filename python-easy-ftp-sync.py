acac60c438171e0ccbb0028baa9fdb155e9fc802
Jimmy J. Olano S <jimmy@ks7000.net.ve>
2020-01-16 23:45:51 -0400

    Getting file name with ftp.nlst() and downloading it

--- python-easy-ftp-sync.py
+++ python-easy-ftp-sync.py
@@ -1,6 +1,12 @@
 from ftplib import FTP
 ftp = FTP('192.168.1.47')
 ftp.login('usuario','contraseña')
-ftp.cwd('/home/jimmy/Descargas')
-ftp.dir()
-ftp.quit()
+ftp.cwd('/home/jimmy/Descargas/BCV')
+
+miLista = ftp.nlst()
+archivo = open(miLista[0], 'wb')
+ftp.retrbinary('RETR ' + miLista[0], archivo.write)
+archivo.close()
+
+ftp.close()
+ftp = None
