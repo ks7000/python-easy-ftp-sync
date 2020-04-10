06fefe5ffa5803b52d2f6b5b856d5105f04ddd2e
Jimmy J. Olano S <jimmy@ks7000.net.ve>
2020-04-10 12:49:40 -0400

    Version 0.1

--- python-easy-ftp-sync.py
+++ python-easy-ftp-sync.py
@@ -15,6 +15,7 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+#    Version 0.1
 def sincronizaFTP(miServidorFTP, miUsuario, miContraseña, miCarpetaRemota, miCarpetaLocal, subir=False, bajar=True):
 
   print("\n-- Conectando y comparando carpetas ----\n")
